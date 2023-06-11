a1d04c195aba052381d9e0a6ab7baddafa02bd8a
MyYo <MyYo@users.noreply.github.com>
2023-06-11 15:18:26 -0700

    First version of main command, might not work

---
+++ mcp_main.py
@@ -0,0 +1,6 @@
+from git_update_code import git_update_code
+
+if __name__ == "__main__":
+    print("Hello World")
+    # Finally, update code for next run
+    git_update_code()
---
+++ scp2_main.py
@@ -0,0 +1,24 @@
+'''
+This file supports backward compatability and will be depricated
+RPi automatically runs this code at start-up.
+This code will check out mcp (if the folder doesn't exist), and run mcp_main.py
+'''
+
+import subprocess
+import os
+
+if __name__ == "__main__":
+    # Get the current directory of the Python script
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Change path to this file's directory
+    os.chdir(current_dir)
+    
+    # Check if mcp-flight-software folder exist
+    if not os.path.isdir("mcp-flight-software"):
+       # Doesn't exist, lets check out code first
+        subprocess.call(["git", "clone", "https://github.com/SpaceHummus/mcp-flight-software.git/trunk/"])
+
+    # Run main code 
+    os.chdir("mcp-flight-software")
+    subprocess.call(["sudo","python","mcp_main.py"])
