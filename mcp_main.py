5b9eb84c5105061ab81c6305970bbc5d5a731dd5
MyYo <MyYo@users.noreply.github.com>
2023-06-11 15:23:06 -0700

    Bug fixes

--- mcp_main.py
+++ mcp_main.py
@@ -1,3 +1,8 @@
+'''
+This is the main function to be executed by linux computer.
+It runs as a "single shot". Collects telemetry, decides what is the next thing to do, then dies.
+'''
+
 from git_update_code import git_update_code
 
 if __name__ == "__main__":
