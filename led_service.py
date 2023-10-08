564e130bc7f5580a361b98023d48ce9f70b6ac5a
MyYo <MyYo@users.noreply.github.com>
2023-10-08 12:11:50 -0700

    Enable LED channel

--- led_service.py
+++ led_service.py
@@ -11,15 +11,17 @@ import time
 pixels = neopixel.NeoPixel(board.D21, 10,brightness =1)
 tel = TelemetryHandler
 
+GPIO.setmode(GPIO.BCM)
+LED_ENABLE_PIN = 23
+GPIO.setup(LED_ENABLE_PIN,GPIO.OUT)
+
 # Returns current led mode (mode_name,r,g,b)
 def get_led_mode():
-    # Todo, read from file
-
     return led_mode_report.get_led_mode_from_file()
 
 def set_led_mode(new_r, new_g, new_b, new_mode_name):
     
-    # Read swhat is the current mode
+    # Read what is the current mode
     current_mode_name, current_r, current_g, current_b = get_led_mode()
     
     # Compare current mode to new mode, if they are the same, we are done
@@ -27,7 +29,7 @@ def set_led_mode(new_r, new_g, new_b, new_mode_name):
         
         # If new mode is off, turn off LED power by switching off the GPIO
         if new_mode_name == "off":
-            print("TODO") # Todo
+        GPIO.output(LED_ENABLE_PIN, False)# Turn Neo pixel enable line
         
         # Nothing more to do, return
         return
@@ -37,10 +39,10 @@ def set_led_mode(new_r, new_g, new_b, new_mode_name):
     
     # Set GPIO switch according to mode
     if new_mode_name == "off":
-        print("TODO") # Todo, trurn GPIO
+        GPIO.output(LED_ENABLE_PIN, False)# Turn Neo pixel enable line
     else:
         # Turn on GPIO
-        print("TODO")
+        GPIO.output(LED_ENABLE_PIN, True)# Turn on Neo pixel enable line
         
         # Let power ramp up
         time.sleep(0.1)
@@ -59,9 +61,16 @@ def set_led_mode(new_r, new_g, new_b, new_mode_name):
 # Test functionality
 if __name__ == "__main__":
     setup_logging()
-    
-    # Todo
-
-
+    set_led_mode("off",0,0,0)
+    time.sleep(0.5)
+    set_led_mode("red",255,0,0)
+    time.sleep(0.5)
+    set_led_mode("green",0,255,0)
+    time.sleep(0.5)
+    set_led_mode("blue",0,0,255)
+    time.sleep(0.5)
+    set_led_mode("white",255,255,255)
+    time.sleep(0.5)
+    set_led_mode("off",0,0,0)
 
 
